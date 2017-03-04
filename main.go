15db4f67c6f8f86e369a0146610e2c608a5e9e24
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-03-04 12:39:39 +0000

    Add search and CLI args, refactoring too

--- main.go
+++ main.go
@@ -2,49 +2,101 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"path"
 
+	terminalGiphy "github.com/bkazi/Terminal-GIPHY/lib"
 	imgcat "github.com/martinlindhe/imgcat/lib"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 const apiParam = "dc6zaTOxFJmzC"
 const apiKey = "api_key"
 
-const trendingURL = "http://api.giphy.com/v1/gifs/trending"
+const apiURL = "http://api.giphy.com/v1/gifs/"
 
-func main() {
+var (
+	app = kingpin.New("Terminal-GIPHY", "A terminal client for GIPHY")
 
-	u, err := url.Parse(trendingURL)
+	trending = app.Command("trending", "Look at trending GIFs")
+
+	search = app.Command("search", "Search for GIFs")
+	query  = search.Arg("query", "Search string").Required().String()
+)
+
+func constructURL(endpoint string, extraKey string, extraParam string) (*url.URL, error) {
+	u, err := url.Parse(apiURL)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
+	u.Path = path.Join(u.Path, endpoint)
 	q := u.Query()
 	q.Add(apiKey, apiParam)
+	q.Add("limit", "1")
+	q.Add(extraKey, extraParam)
 	u.RawQuery = q.Encode()
+	return u, nil
+}
 
-	var responseJSON map[string]interface{}
+func main() {
+	var (
+		endpoint   string
+		extraKey   string
+		extraParam string
+	)
+
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+
+	case trending.FullCommand():
+		endpoint = "trending"
+
+	case search.FullCommand():
+		endpoint = "search"
+		extraKey = "q"
+		extraParam = *query
+	}
+
+	u, err := constructURL(endpoint, extraKey, extraParam)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	response := terminalGiphy.Response{}
 	resp, err := http.Get(u.String())
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 	defer resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
-	error := json.Unmarshal(body, &responseJSON)
-	if error != nil {
-		log.Fatal(error)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	data := responseJSON["data"].([]interface{})
-	gif := data[0].(map[string]interface{})
-	gifImageData := gif["images"].(map[string]interface{})
-	gifURL := gifImageData["fixed_height"].(map[string]interface{})["url"].(string)
+	if len(response.Data) == 0 {
+		fmt.Println("No GIFs found :(")
+		os.Exit(1)
+	}
+	gifImageData := response.Data[0].Images
+	gifURL := gifImageData.FixedHeight["url"].(string)
 
-	gifResp, _ := http.Get(gifURL)
+	gifResp, err := http.Get(gifURL)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	defer gifResp.Body.Close()
 
 	imgcat.Cat(gifResp.Body, os.Stdout)
